147af9cb4e8081ac91a746ce5c6962b6dc9d33c1
Tanush Sethi <tanushsethi55@gmail.com>
2022-09-10 20:33:30 +0530

    meme images changing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,24 @@
 import React from "react";
+import memesData from "../data";
 
 function Main(){
+    const [memeData , setMeme] = React.useState({
+        url: "https://i.imgflip.com/24y43o.jpg",
+        topText: "",
+        bottomText: ""
+    });
+    const [dataObj, setAllMemeImages] = React.useState(memesData)
+    var {data: {memes}} = dataObj;
+    function generate(){
+        var rand = Math.floor(Math.random() * memes.length);
+        setMeme(function(current){
+            return {
+                ...current,
+                url : memes[rand].url
+            }
+        })
+    }
+
     return(
         <div className="main">
             <div className="inputCon">
@@ -8,7 +26,10 @@ function Main(){
                 <input className="input" type="text" placeholder="Bottom Text"/>
             </div>
             <div className="buttonCon">
-                <button>Get a new meme image! </button>
+                <button onClick={generate}>Get a new meme image! </button>
+            </div>
+            <div className="imgCon">
+                <img src={memeData.url} alt="" />
             </div>
         </div>
     );
